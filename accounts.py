5f97689affc8580709b1d83e6d51f88235dc9fde
h78man99-a11y <h78man99@gmail.com>
2026-01-02 18:10:48 +0530

    Add second account configuration to accounts.py

--- accounts.py
+++ accounts.py
@@ -11,5 +11,16 @@ ACCOUNTS = [
         "REFRESH_TOKEN": "AMf-vBzRCLBIhyUpGyF5vxS8C3yg-cMxx1CRojWNKWkdk66kq8L-cr6Sj71d3qy0VkJtJXUyogRIW8UPUeeoZ2ZXeeUZWhTsGbFiQl1BotnWQ3A6-6114LgldHFxe8Z-GBKgvtnxV8X_kXdMfDKeWR0eHBGBzNDB7Sz5wA0g2bvyjxA1lJMqG77_7iHtFUztCN5NOWxLrkeiLxobFFyx9wGaO_5sdVJwTqorJt53PMsFjEfwTlC4P_lfw6xFO_H7NqsqXOMEUI4UyZWn9sByWR9ZJ7_HB12q3PPNnQ-MmAaQNQtYF2MragYn8fxjoOyQrKNH7QxelQr8PEbg_Np2htpcwqp-HJul-xE-a_DiYJFH7thOx3-M7ZZO2RiI6bi7ger8zIEngO8I5VkgTrCGlb147bIgYqOur1hWLS88wJZNmxap9ndYtd0",
         "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
         "SPOT_ID": "2759509"
-    }
+    },
+
+      {
+        "NAME": "Chutiya",
+        "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/7d2266f18c9c43ba888830f51cb98e8f/raw/4ee0b19a90c3055f6b4ff7f0348355f8b601815e/adba.json",
+        "FIREBASE_KEY": "AIzaSyBBK7rf67OjPHTEVNRWjFglPMKTF4Ze2kQ",
+        "PROJECT_ID": "cash-romeo",
+        "REFRESH_TOKEN": "AMf-vBzRCLBIhyUpGyF5vxS8C3yg-cMxx1CRojWNKWkdk66kq8L-cr6Sj71d3qy0VkJtJXUyogRIW8UPUeeoZ2ZXeeUZWhTsGbFiQl1BotnWQ3A6-6114LgldHFxe8Z-GBKgvtnxV8X_kXdMfDKeWR0eHBGBzNDB7Sz5wA0g2bvyjxA1lJMqG77_7iHtFUztCN5NOWxLrkeiLxobFFyx9wGaO_5sdVJwTqorJt53PMsFjEfwTlC4P_lfw6xFO_H7NqsqXOMEUI4UyZWn9sByWR9ZJ7_HB12q3PPNnQ-MmAaQNQtYF2MragYn8fxjoOyQrKNH7QxelQr8PEbg_Np2htpcwqp-HJul-xE-a_DiYJFH7thOx3-M7ZZO2RiI6bi7ger8zIEngO8I5VkgTrCGlb147bIgYqOur1hWLS88wJZNmxap9ndYtd0",
+        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
+        "SPOT_ID": "2759511"
+      }
 ]
+
