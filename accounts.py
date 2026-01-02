6544cf3c51b3160b16d16c0f7d6e7097ee04ba4b
h78man99-a11y <h78man99@gmail.com>
2026-01-02 17:57:01 +0530

    Add files via upload

---
+++ accounts.py
@@ -0,0 +1,15 @@
+# accounts.py
+# ================= MULTI ACCOUNT CONFIG =================
+
+ACCOUNTS = [
+
+   {
+        "NAME": "Chutiya",
+        "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/57611a91dbfb2558b1169e9da2e80da9/raw/d8c55f29ab0e8f593299a91c14307bc63d333082/ada.json",
+        "FIREBASE_KEY": "AIzaSyBBK7rf67OjPHTEVNRWjFglPMKTF4Ze2kQ",
+        "PROJECT_ID": "cash-romeo",
+        "REFRESH_TOKEN": "AMf-vBzRCLBIhyUpGyF5vxS8C3yg-cMxx1CRojWNKWkdk66kq8L-cr6Sj71d3qy0VkJtJXUyogRIW8UPUeeoZ2ZXeeUZWhTsGbFiQl1BotnWQ3A6-6114LgldHFxe8Z-GBKgvtnxV8X_kXdMfDKeWR0eHBGBzNDB7Sz5wA0g2bvyjxA1lJMqG77_7iHtFUztCN5NOWxLrkeiLxobFFyx9wGaO_5sdVJwTqorJt53PMsFjEfwTlC4P_lfw6xFO_H7NqsqXOMEUI4UyZWn9sByWR9ZJ7_HB12q3PPNnQ-MmAaQNQtYF2MragYn8fxjoOyQrKNH7QxelQr8PEbg_Np2htpcwqp-HJul-xE-a_DiYJFH7thOx3-M7ZZO2RiI6bi7ger8zIEngO8I5VkgTrCGlb147bIgYqOur1hWLS88wJZNmxap9ndYtd0",
+        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
+        "SPOT_ID": "2759509"
+    }
+]
