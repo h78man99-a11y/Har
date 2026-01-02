8e6ae8f1057f6520c02b959ef3bc95e9d586f330
h78man99-a11y <h78man99@gmail.com>
2026-01-02 20:58:06 +0530

    Update account configurations and URLs

--- accounts.py
+++ accounts.py
@@ -5,23 +5,12 @@ ACCOUNTS = [
 
    {
         "NAME": "Chutiya",
-        "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/57611a91dbfb2558b1169e9da2e80da9/raw/d8c55f29ab0e8f593299a91c14307bc63d333082/ada.json",
-        "FIREBASE_KEY": "AIzaSyBBK7rf67OjPHTEVNRWjFglPMKTF4Ze2kQ",
-        "PROJECT_ID": "cash-romeo",
-        "REFRESH_TOKEN": "AMf-vBzRCLBIhyUpGyF5vxS8C3yg-cMxx1CRojWNKWkdk66kq8L-cr6Sj71d3qy0VkJtJXUyogRIW8UPUeeoZ2ZXeeUZWhTsGbFiQl1BotnWQ3A6-6114LgldHFxe8Z-GBKgvtnxV8X_kXdMfDKeWR0eHBGBzNDB7Sz5wA0g2bvyjxA1lJMqG77_7iHtFUztCN5NOWxLrkeiLxobFFyx9wGaO_5sdVJwTqorJt53PMsFjEfwTlC4P_lfw6xFO_H7NqsqXOMEUI4UyZWn9sByWR9ZJ7_HB12q3PPNnQ-MmAaQNQtYF2MragYn8fxjoOyQrKNH7QxelQr8PEbg_Np2htpcwqp-HJul-xE-a_DiYJFH7thOx3-M7ZZO2RiI6bi7ger8zIEngO8I5VkgTrCGlb147bIgYqOur1hWLS88wJZNmxap9ndYtd0",
+        "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/c7509acad298ba225e761d9a0f5eaf4d/raw/dc67ab285b9350b56fd82f24757e4b1fef25d655/Jia.json",
+        "FIREBASE_KEY": "AIzaSyAkSwrPZkuDYUGWU65NAVtbidYzE5ydIJ4",
+        "PROJECT_ID": "cash-rhino",
+        "REFRESH_TOKEN": "AMf-vBzDByo-Nd1YRf9h3s2LqJoPJ4EAksAhb6t5lydso8bF7-YpesdVSWyB5GuxLDMR_-MHSRnVvakoRwEM50KTY6CY4QKe_jXe3qrbLo2qaHgOx_ZqcHH_lqRFbrhKps-j8t4kyjlU1WtMvrXOTmQwj3470lWvt1wvw1teWVS_CpIFMmmVB8BhnzZI_6l2ey5e_NV06Kq1xcXg--jocRdjpa_7iolCjS-3IU_UF9VfLoax2R-8gH9PFi_kCG0kk2Q5ZdwsjUPwibvoQYpI7TpCNJ1yO4StWAALhpTsjfVj8C7-9ofiiRK-4rtIrUjTw1d1Qowv7u8OWK9_5gPfUkQ-3y56NNt7j5NFZ2Ow0Yz1iNsutFbUlLnESy9TkrFiTG2qJmql3QclYyW2N_ezgqFHnB0FxPfjuYE06ads6nxlRRsHskXZPUg",
         "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2759509"
-    },
-
-      {
-        "NAME": "Chuiya",
-        "JSON_URL": "https://gist.githubusercontent.com/h78man99-a11y/7d2266f18c9c43ba888830f51cb98e8f/raw/4ee0b19a90c3055f6b4ff7f0348355f8b601815e/adba.json",
-        "FIREBASE_KEY": "AIzaSyBBK7rf67OjPHTEVNRWjFglPMKTF4Ze2kQ",
-        "PROJECT_ID": "cash-romeo",
-        "REFRESH_TOKEN": "AMf-vBzRCLBIhyUpGyF5vxS8C3yg-cMxx1CRojWNKWkdk66kq8L-cr6Sj71d3qy0VkJtJXUyogRIW8UPUeeoZ2ZXeeUZWhTsGbFiQl1BotnWQ3A6-6114LgldHFxe8Z-GBKgvtnxV8X_kXdMfDKeWR0eHBGBzNDB7Sz5wA0g2bvyjxA1lJMqG77_7iHtFUztCN5NOWxLrkeiLxobFFyx9wGaO_5sdVJwTqorJt53PMsFjEfwTlC4P_lfw6xFO_H7NqsqXOMEUI4UyZWn9sByWR9ZJ7_HB12q3PPNnQ-MmAaQNQtYF2MragYn8fxjoOyQrKNH7QxelQr8PEbg_Np2htpcwqp-HJul-xE-a_DiYJFH7thOx3-M7ZZO2RiI6bi7ger8zIEngO8I5VkgTrCGlb147bIgYqOur1hWLS88wJZNmxap9ndYtd0",
-        "BASE_URL": "https://fairbid.inner-active.mobi/simpleM2M/fyberMediation",
-        "SPOT_ID": "2759511"
-      }
+        "SPOT_ID": "2759495"
+    }
 ]
-
 
